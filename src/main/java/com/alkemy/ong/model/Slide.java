9ebd578bd9fcf96177df71ebcec61445a9ba0b80
Mateo Lopez <mateolopez27.11@gmail.com>
2021-11-25 13:57:31 -0300

    add dependency and change model slide

--- src/main/java/com/alkemy/ong/model/Slide.java
+++ src/main/java/com/alkemy/ong/model/Slide.java
@@ -5,95 +5,42 @@
  */
 package com.alkemy.ong.model;
 
+
+
+import com.sun.istack.NotNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+
 
 
 /**
  *
  * @author mateo
  */
+@Data
 @Entity
+@Table(name="slide")
 public class Slide {
     
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    
+    @NotNull(message = "El url no puede estar vacio")
     private String imageUrl;
+    
+    @NotNull(message="El text no puede estar vacio")
     private String text;
+    
+    @NotNull
     private Integer order;
+    
+    @NotNull(message="organizationId no puede estar vacio")
     private Long organizationId;
-
-    /**
-     * @return the id
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
-     * @return the imageUrl
-     */
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    /**
-     * @param imageUrl the imageUrl to set
-     */
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    /**
-     * @return the text
-     */
-    public String getText() {
-        return text;
-    }
-
-    /**
-     * @param text the text to set
-     */
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    /**
-     * @return the order
-     */
-    public Integer getOrder() {
-        return order;
-    }
-
-    /**
-     * @param order the order to set
-     */
-    public void setOrder(Integer order) {
-        this.order = order;
-    }
-
-    /**
-     * @return the organizationId
-     */
-    public Long getOrganizationId() {
-        return organizationId;
-    }
-
-    /**
-     * @param organizationId the organizationId to set
-     */
-    public void setOrganizationId(Long organizationId) {
-        this.organizationId = organizationId;
-    }
-  
+    
 }
