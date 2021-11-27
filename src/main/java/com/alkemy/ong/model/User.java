47ab948390abdf63b6b6aeb51539e864db426683
Jose Alejandro <62119845+alejandroitsoluciones@users.noreply.github.com>
2021-11-27 13:22:03 -0300

    UserDetails interface was removed and field for token was added

---
+++ src/main/java/com/alkemy/ong/model/User.java
@@ -0,0 +1,60 @@
+package com.alkemy.ong.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@SQLDelete(sql = "UPDATE user SET enabled = false WHERE userId = ?")
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotNull(message = "First name cannot be null")
+    private String firstName;
+
+    @NotNull(message = "Last name cannot be null")
+    private String lastName;
+
+    @NotNull(message = "Email cannot be null")
+    private String email;
+
+    @NotNull(message = "Password cannot be null")
+    private String password;
+
+    @Nullable
+    private String photo;
+
+    @NotNull
+    private Boolean enabled=true;
+
+    @CreatedDate
+    @NotNull
+    @Column(updatable=false)
+    private LocalDateTime dateCreation;
+
+    @LastModifiedDate
+    private LocalDateTime dateUpdate;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="roleId")
+    private Role role;
+
+    @Transient
+    private String token;
+}
