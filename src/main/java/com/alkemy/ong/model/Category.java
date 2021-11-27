06e26bb2a30151012faeee081fae0e1f004d2b0d
Jose Alejandro <62119845+alejandroitsoluciones@users.noreply.github.com>
2021-11-27 13:50:56 -0300

    Fixed type of id data in Category entity

--- src/main/java/com/alkemy/ong/model/Category.java
+++ src/main/java/com/alkemy/ong/model/Category.java
@@ -16,7 +16,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     @NotNull(message = "Name cannot be null")
     private String name;
     private String description;
