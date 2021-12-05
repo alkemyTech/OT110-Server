e1d236652958b15e74f67c59a9a6fc7b57e1d971
Jose Alejandro <62119845+alejandroitsoluciones@users.noreply.github.com>
2021-12-04 21:45:13 -0300

    Add logic for category update in [Category ServiceImpl]

--- src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
@@ -6,11 +6,17 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.CategoryDto;
+import com.alkemy.ong.dto.CategoryRequestUpdate;
 import com.alkemy.ong.model.Category;
 import com.alkemy.ong.repository.CategoryRepository;
 import com.alkemy.ong.service.ICategoryService;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
-import lombok.AllArgsConstructor;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,11 +24,10 @@ import org.springframework.stereotype.Service;
  * @author mateo
  */
 @Service
-@AllArgsConstructor
 public class CategoryServiceImpl implements ICategoryService{
     
-    public  final CategoryRepository categoryRepository;
-    
+	@Autowired
+    private CategoryRepository categoryRepository;
     
     @Override
     public CategoryDto findById(Long id){
@@ -34,9 +39,42 @@ public class CategoryServiceImpl implements ICategoryService{
             categoryDto.setImage(category.getImage());
             categoryDto.setDescripcion(category.getDescription());
             categoryDto.setName(category.getName());
-            categoryDto.setDateCreacion(category.getDateCreation());
-            categoryDto.setDateUpdate(category.getDateUpdate());
+            categoryDto.setDateCreacion(category.getDateCreation().toString());
+            categoryDto.setDateUpdate(category.getDateUpdate().toString());
         }
         return categoryDto;
     }
+
+
+	@Override
+	public CategoryDto updateCategory(@Valid CategoryRequestUpdate category, Long id) {
+		Optional<Category> existCategory = categoryRepository.findById(id);
+		if (existCategory.isPresent()) {
+			Category categoryEntity = categoryRepository.findById(id).get();
+			mapDtoToEntity(categoryEntity, category);
+			Category categoryUpdated = categoryRepository.save(categoryEntity);
+			return mapEntityToDto(categoryUpdated);
+		}
+		return null;
+	}
+
+
+	private CategoryDto mapEntityToDto(Category categoryUpdated) {
+		CategoryDto categoryDto = new CategoryDto();
+		categoryDto.setId(categoryUpdated.getId());
+		categoryDto.setName(categoryUpdated.getName());
+		categoryDto.setDescripcion(categoryUpdated.getDescription());
+		categoryDto.setImage(categoryUpdated.getImage());
+		categoryDto.setDateCreacion(categoryUpdated.getDateCreation().toString());
+		categoryDto.setDateUpdate(categoryUpdated.getDateUpdate().toString());
+		return categoryDto;
+	}
+
+
+	private void mapDtoToEntity(Category categoryEntity, @Valid CategoryRequestUpdate category) {
+		categoryEntity.setName(category.getName());
+		categoryEntity.setDescription(category.getDescripcion());
+		categoryEntity.setImage(category.getImage());
+		categoryEntity.setDateUpdate(LocalDateTime.now());
+	}
 }
