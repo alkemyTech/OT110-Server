2998c00293456130ff5eb4eb4decf233b41c57b7
Jose Alejandro <alejandroitsoluciones@gmail.com>
2021-12-15 22:29:37 -0300

    A method that returns pagination is added in the interface

--- src/main/java/com/alkemy/ong/service/ICategoryService.java
+++ src/main/java/com/alkemy/ong/service/ICategoryService.java
@@ -8,12 +8,13 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.CategoryByNameDto;
 import javax.validation.Valid;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import com.alkemy.ong.dto.CategoryDto;
 import com.alkemy.ong.dto.CategoryRequestUpdate;
-
-
-
+import com.alkemy.ong.model.Category;
 
 import java.util.List;
 
@@ -30,6 +31,8 @@ public interface ICategoryService {
     CategoryDto findById(Long id);
     
 	CategoryDto updateCategory(@Valid CategoryRequestUpdate category, Long id);
+
+	Page<Category> readAllCategoriesByName(Pageable pageable, int page);
 
 
 }
