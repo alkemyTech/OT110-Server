fc52d5005092b154fcb2e3b260ab370ef665fe77
Jose Alejandro <alejandroitsoluciones@gmail.com>
2021-12-19 13:31:16 -0300

    updateUserRole() method is added in the interface

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -3,11 +3,19 @@ package com.alkemy.ong.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.alkemy.ong.model.Role;
 import com.alkemy.ong.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
+	
+    @Modifying
+    @Query("UPDATE User SET role = :role WHERE email = :email")
+	void updateUserRole(@Param("email") String email, @Param("role") Role role);
 }
