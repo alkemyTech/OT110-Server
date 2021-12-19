0df6ff36b640c45862a8b866947566344ec84782
Jose Alejandro <alejandroitsoluciones@gmail.com>
2021-12-19 19:53:13 -0300

    SYSTEM_MANAGER is added

--- src/main/java/com/alkemy/ong/security/SecurityConstant.java
+++ src/main/java/com/alkemy/ong/security/SecurityConstant.java
@@ -5,4 +5,6 @@ public interface SecurityConstant {
 	public static final String ADMIN = "hasAnyRole(T(com.alkemy.ong.security.RoleEnum).ADMIN)";
 	public static final String USER = "hasAnyRole(T(com.alkemy.ong.security.RoleEnum).USER)";
 	public static final String USER_ADMIN = USER + " || " + ADMIN;
+	public static final String SYSTEM_MANAGER = "hasAnyRole(T(com.alkemy.ong.security.RoleEnum).SYSTEM_MANAGER)";
+	
 }
