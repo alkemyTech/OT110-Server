287d02db00419b9f7354321b823a1d10030ab338
Jose Alejandro <alejandroitsoluciones@gmail.com>
2021-12-19 19:54:18 -0300

    @PreAuthorize is added with SYSTEM_MANAGER role

--- src/main/java/com/alkemy/ong/security/controller/InternalApiController.java
+++ src/main/java/com/alkemy/ong/security/controller/InternalApiController.java
@@ -2,11 +2,13 @@ package com.alkemy.ong.security.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alkemy.ong.security.SecurityConstant;
 import com.alkemy.ong.service.IUserService;
 
 import lombok.RequiredArgsConstructor;
@@ -19,6 +21,7 @@ public class InternalApiController {
 	private final IUserService userService;
 	
     @PutMapping("/admin/{email}")
+    @PreAuthorize(SecurityConstant.SYSTEM_MANAGER)
     public ResponseEntity<?> makeAdmin(@PathVariable String email){
         String message = userService.makeAdmin(email);
         return new ResponseEntity<>(message, HttpStatus.OK);
