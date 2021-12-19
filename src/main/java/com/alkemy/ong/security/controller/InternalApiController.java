9c9e356eb8804deb869a79ca3ca3e07dad4aee4b
Jose Alejandro <alejandroitsoluciones@gmail.com>
2021-12-19 13:33:22 -0300

    Endpoint /api/v1/internal is added

---
+++ src/main/java/com/alkemy/ong/security/controller/InternalApiController.java
@@ -0,0 +1,27 @@
+package com.alkemy.ong.security.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alkemy.ong.service.IUserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/internal")
+@RequiredArgsConstructor
+public class InternalApiController {
+
+	private final IUserService userService;
+	
+    @PutMapping("/admin/{email}")
+    public ResponseEntity<?> makeAdmin(@PathVariable String email){
+        userService.makeAdmin(email);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }	
+	
+}
