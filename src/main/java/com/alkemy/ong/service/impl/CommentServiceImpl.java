cc6286cb586b744f8f933a80992d0f27a3562ca8
Valle1989 <fedevalle27@gmail.com>
2021-12-20 21:19:41 -0300

    CommentServiceImpl modified

--- src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
@@ -3,10 +3,12 @@ package com.alkemy.ong.service.impl;
 import com.alkemy.ong.dto.CommentRequest;
 import com.alkemy.ong.dto.CommentResponse;
 import com.alkemy.ong.exception.EmptyDataException;
+import com.alkemy.ong.exception.NotFoundException;
 import com.alkemy.ong.model.Comment;
 import com.alkemy.ong.model.News;
 import com.alkemy.ong.model.User;
 import com.alkemy.ong.repository.CommentRepository;
+import com.alkemy.ong.repository.NewsRepository;
 import com.alkemy.ong.service.ICommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class CommentServiceImpl implements ICommentService {
 
     private final CommentRepository commentRepository;
     private final UserServiceImpl userService;
+    private final NewsRepository newsRepository;
     private final NewsServiceImpl newsService;
     private final MessageSource messageSource;
 
@@ -42,22 +46,24 @@ public class CommentServiceImpl implements ICommentService {
 
     @Override
     public List<CommentResponse> getAllComments(Long id) {
-        String contactListEmpty = messageSource.getMessage("contact.listEmpty",null, Locale.US);
+        String newsNotFound = messageSource.getMessage("news.notFound",null, Locale.US);
 
+        Optional<News> existNews = newsRepository.findById(id);
         List<CommentResponse> comments = new ArrayList<>();
 
-        commentRepository.findAll()
-                .stream()
-                .filter(comment -> comment.getNews().getId() == id)
-                .forEach(comment -> {
-                    CommentResponse commentResponse = new CommentResponse();
-                    commentResponse.setUserId(comment.getUser().getUserId());
-                    commentResponse.setBody(comment.getBody());
-                    commentResponse.setNewsId(comment.getNews().getId());
-                    comments.add(commentResponse);
-                });
-        if(comments.isEmpty()){
-            throw new EmptyDataException(contactListEmpty);
+        if(existNews.isPresent()){
+            commentRepository.findAll()
+                    .stream()
+                    .filter(comment -> comment.getNews().getId() == id)
+                    .forEach(comment -> {
+                        CommentResponse commentResponse = new CommentResponse();
+                        commentResponse.setUserId(comment.getUser().getUserId());
+                        commentResponse.setBody(comment.getBody());
+                        commentResponse.setNewsId(comment.getNews().getId());
+                        comments.add(commentResponse);
+                    });
+        } else{
+            throw new NotFoundException(newsNotFound);
         }
         return comments;
     }
